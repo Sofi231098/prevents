6575fac7304c3fb537c54d583604b8216bb38001
sofia trinidad yañez <abrilyanez123@gmail.com>
2025-02-26 16:47:18 -0300

    feature: se crea el event item

--- src/modules/events/components/event-item/event-item.tsx
+++ src/modules/events/components/event-item/event-item.tsx
@@ -2,8 +2,12 @@ import styles from './event-item.module.css';
 
 export const EventItem = () => {
     return (
-        <div className={styles.test}>
-            event-item
-        </div>
+        <article className={styles.item}>
+            <img 
+            className={styles.imgItem} 
+            width={300}
+            src="https://www.cnet.com/a/img/resize/6019f01d3b92576edc0a52712a047db8cb78331d/hub/2014/10/27/40194e6e-2544-419e-a340-f7c17b2e83c5/html5-wow-image-w3c.jpg?auto=webp&fit=crop&height=900&width=1200" alt=""/>
+            <p className={styles.itemName}>Nombre del evento bien largo para probar</p>
+        </article>
     )
 }
