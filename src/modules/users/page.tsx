cc7a37ad07c019fabe617a7ad0a599509dda0d08
sofia trinidad yañez <abrilyanez123@gmail.com>
2025-03-12 16:22:55 -0300

    feature: componente Tabs para la página Profile con react-router y estilos activos

--- src/modules/users/page.tsx
+++ src/modules/users/page.tsx
@@ -1,9 +1,22 @@
+import { Tabs } from "@/shared/components/tabs/tabs";
 import { Outlet } from "react-router";
+
+const listaTabs = [
+    {
+        label: 'Mi información',
+        path: 'my-info'
+    },
+    {
+        label: 'Eventos favoritos',
+        path: 'liked-events'
+    }
+]
 
 const UsersPage = () => {
     return (
         <>
             <h1>Profile</h1>
+            <Tabs tabs={listaTabs} />
             <Outlet />
         </>
     )
